057597d81491f7f75c03e9add5961e235af5e5fc
Jcano99 <joancanojulia@gmail.com>
2026-04-21 10:44:46 +0200

    feat: repo otn-webs — can-garriga i mariagifra

---
+++ mariagifra/astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  server: { port: 4322 },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
